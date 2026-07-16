1033af6820373ebcf24af6869732df48309b52fe
OrangeHance <510519803@qq.com>
2026-07-16 20:41:04 +0800

    整理代码

---
+++ database/carcheckitems.sql
@@ -0,0 +1,5 @@
+INSERT INTO sys.carcheckitems (`desc`,url,`create`,createTime,`update`,updateTime) VALUES
+	 ('检查螺丝','../../assets/image/LS.png',NULL,NULL,NULL,NULL),
+	 ('检查轮胎','../../assets/image/LT.png',NULL,NULL,NULL,NULL),
+	 ('检查玻璃','../../assets/image/BL.png',NULL,NULL,NULL,NULL),
+	 ('检查车漆','../../assets/image/CQ.png',NULL,NULL,NULL,NULL);
--- database/menu.sql
+++ database/menu.sql
@@ -1,5 +1,8 @@
-INSERT INTO menu (menu_name,menu_url,create_time) VALUES
+INSERT INTO sys.menu (menu_name,menu_url,create_time) VALUES
 	 ('首页','/','2026-05-17 00:00:00'),
-	 ('库存管理','/wareHouse','2026-05-17 00:00:00'),
-	 ('入库管理','/wareHouseIn','2026-05-17 00:00:00'),
-	 ('出库管理','/wareHouseOut','2026-05-17 00:00:00');
+	 ('A岗位','/aJobMenu','2026-05-17 00:00:00'),
+	 ('B岗位','/bJobMenu','2026-05-17 00:00:00'),
+	 ('C岗位','/cJobMenu','2026-05-17 00:00:00'),
+	 ('D岗位','/dJobMenu','2026-05-17 00:00:00'),
+	 ('E岗位','/eJobMenu','2026-05-17 00:00:00'),
+	 ('F岗位','/fJobMenu','2026-05-17 00:00:00'),
